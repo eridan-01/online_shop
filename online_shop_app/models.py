6f60b31a28bd90ec85be6d743f94cee085483040
Eugene Geym <geymzheka@gmail.com>
2024-05-13 14:00:42 +0500

    added classes Product and Category

--- online_shop_app/models.py
+++ online_shop_app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    name_of_category = models.CharField(
+        max_length=100, verbose_name="Название категории"
+    )
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+
+    def __str__(self):
+        return f"{self.name_of_category}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+    preview = models.ImageField(
+        upload_to="online_shop_app/products",
+        verbose_name="Изображение (превью)",
+        **NULLABLE,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория товара",
+        **NULLABLE,
+        related_name="products",
+    )
+    price = models.IntegerField()
+    created_at = models.DateTimeField(
+        blank=True, verbose_name="Дата создания (записи в БД)"
+    )
+    updated_at = models.DateTimeField(
+        blank=True, verbose_name="Дата последнего изменения (записи в БД)"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
