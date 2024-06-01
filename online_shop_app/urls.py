34f7814c319126e7d146ac480693c6eeb4ba6ab2
Eugene Geym <geymzheka@gmail.com>
2024-06-01 18:17:50 +0500

    added tags for photo

--- online_shop_app/urls.py
+++ online_shop_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from online_shop_app.apps import OnlineShopAppConfig
-from online_shop_app.views import home, contacts
+from online_shop_app.views import home, contacts, products_detail
 
 app_name = OnlineShopAppConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>', products_detail, name='products_detail')
 ]
--- online_shop_app/views.py
+++ online_shop_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from online_shop_app.models import Product
 
@@ -11,3 +11,9 @@ def home(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'products_detail.html', context)
