dbe9ec063e13a937d2b5a14baaac8b5ec4dda746
Eugene Geym <geymzheka@gmail.com>
2024-06-01 15:54:49 +0500

    changed staticfiles_dirs

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('online_shop_app.urls', namespace='online_shop_app'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
