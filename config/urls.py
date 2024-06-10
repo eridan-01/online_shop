06847d0f88a1de07370134afaa8d413fa369b4b2
Eugene Geym <geymzheka@gmail.com>
2024-06-10 21:18:34 +0500

    added app in sittings, added urls blog

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('online_shop_app.urls', namespace='online_shop_app'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('online_shop_app.urls', namespace='online_shop_app')),
+                  path('blog/', include('blog.urls', namespace='blog'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
