e9d66f119ba60290b41fd75a041a5b335b18dbfc
Eugene Geym <geymzheka@gmail.com>
2024-06-18 16:17:00 +0500

    added views count

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,12 @@ class BlogArticleListView(ListView):
 class BlogArticleDetailView(DetailView):
     model = BlogArticle
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogArticleCreateView(CreateView):
     model = BlogArticle
