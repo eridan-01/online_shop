3d8c72cbbc748b1e8b5c55ed73d18bfd005662dc
Eugene Geym <geymzheka@gmail.com>
2024-06-28 03:36:18 +0500

    changed nullable

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-from online_shop_app.models import NULLABLE
+NULLABLE = {"blank": True, "null": True}
 
 
 class User(AbstractUser):
