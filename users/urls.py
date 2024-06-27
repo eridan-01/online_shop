9bbcf5cc58dda8336ff63d56cdf941e7cb0a2416
Eugene Geym <geymzheka@gmail.com>
2024-06-28 03:19:03 +0500

    added password reset

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserCreateView, email_verification
+from users.views import UserCreateView, email_verification, PasswordResetView
 
 app_name = UsersConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 import secrets
+import string
 
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import CreateView
 
 from users.forms import UserRegisterForm
@@ -37,4 +41,36 @@ def email_verification(request, token):
     user.is_active = True
     user.save()
     return redirect(reverse('users:login'))
+
+
+def generate_random_password(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(characters) for _ in range(length))
+
+
+class PasswordResetView(View):
+    def get(self, request):
+        return render(request, 'users/password_reset.html')
+
+    def post(self, request):
+        email = request.POST.get('email')
+        user = User.objects.filter(email=email).first()
+        if user:
+            new_password = generate_random_password()
+            user.password = make_password(new_password)
+            user.save()
+
+            send_mail(
+                subject='Восстановление пароля',
+                message=f'Ваш новый пароль: {new_password}',
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[user.email]
+            )
+
+            messages.success(request, 'Пароль успешно изменён. Пожалуйста, войдите с новым паролем.')
+
+            return redirect('users:login')
+        else:
+            messages.error(request, 'Нет пользователя с таким email.')
+            return redirect('users:password_reset')
 
